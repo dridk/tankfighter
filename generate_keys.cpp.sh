7be72f8e00b7d8840ff0707e694b303189624be7
MetaEntropy <MetaEntropy@gmail.com>
2013-08-22 11:33:26 +0200

    Improved error messages of headers research code.

--- generate_keys.cpp.sh
+++ generate_keys.cpp.sh
@@ -13,18 +13,26 @@ extract_enum() {
 	echo "{NULL, 0}"
 	echo "};"
 }
-find_keyboard_hpp() {
-if [ -n "$SFML_ROOT" ]; then
-	echo "${SFML_ROOT}/include/SFML/Window"
-else
-for d in $(pkg-config --cflags sfml-all;echo /usr/include); do
+find_incl() {
+local libname="$1"
+local headname="$2"
+for d in $(pkg-config --cflags "$libname" 2>/dev/null;echo ${SFML_ROOT}/include/;echo ${SFML_ROOT};echo /usr/local/include;echo /usr/local/include/SFML2;echo /usr/include/SFML2;echo /usr/include); do
 	d=$(echo "$d"|sed 's/^-I//')
-	f="$d/SFML/Window/Keyboard.hpp"
+	f="$d/$headname"
 	if test -e "$f"; then
 		echo "$d/SFML/Window"
 	fi
 done|tail -1
-fi # SFML_ROOT
+}
+find_keyboard_hpp() {
+	path=$(find_incl "sfml-all" "SFML/Window/Keyboard.hpp")
+	if [ -z "$path" ]; then
+		path16=$(find_incl "sfml-1.6" "SFML/Window/WindowListener.hpp")
+		if [ -n "$path16" ]; then
+			echo "SFML 1.6 headers found, but SFML >= 2.0 headers required" >&2
+		fi
+	fi
+	echo "$path"
 }
 
 if [ -z "$kbh" ]; then
@@ -40,6 +48,6 @@ if [ -n "$kbh" ]; then
 	cat keys.cpp.tail
 	exit 0
 else
-	echo "SFML keyboard file not found" >&2
+	echo "SFML 2.0 SFML/Window/Keyboard.hpp file not found. If you find it, set \${SFML_ROOT} so that \${SFML_ROOT}/SFML/Window/Keyboard.hpp exists" >&2
 	exit 1
 fi

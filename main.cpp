03bf55c602d2593595ec86aa33118bc416ff8a2f
MetaEntropy <MetaEntropy@gmail.com>
2013-08-28 18:50:07 +0200

    Now, install target works on Linux.

--- main.cpp
+++ main.cpp
@@ -22,8 +22,15 @@ using namespace sf;
 using namespace std;
 
 #undef DEBUG_OBJECT_OUTLINES
+#define stringify(path) path
 
+static void set_data_directory() {
+#ifndef _WIN32
+	chdir(stringify(TF_PREFIX) "/share/" stringify(TF_NAME) "-" stringify(TF_VERSION));
+#endif
+}
 int main(int argc, char **argv) {
+	set_data_directory();
 	srand(time(NULL));
 	if (!parameters.parseCmdline(argc, argv)) return 0;
 	Engine engine;
